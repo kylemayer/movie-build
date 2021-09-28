e38faa232cb71d650cc342d05549870ee7b21738
Kyle Mayer <x@kylemayer.dev>
2021-09-28 15:41:02 -0700

    init

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9lt2zLNXj8UMVhim-npi0oMzfpcdXHiU",
+  authDomain: "movie-build.firebaseapp.com",
+  projectId: "movie-build",
+  storageBucket: "movie-build.appspot.com",
+  messagingSenderId: "217055421821",
+  appId: "1:217055421821:web:0ec4c35affd6c155921814"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
